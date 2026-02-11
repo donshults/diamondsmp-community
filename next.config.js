ca6575fa075f8deef80aa31613243edb5c56706a
Miles O'Brien (OpenClaw) <miles@openclaw.callteksupport.net>
2026-02-11 02:43:38 +0000

    🚀 Simplify build process to fix hanging issue
    - Revert complex build script that was causing npm install to hang
    - Add postinstall hook for Prisma generation
    - Simplify Next.js config to just externalize Prisma
    - Let Railway use default build process
    
    This should resolve the build timeout issue.

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client'],
   },
-  webpack: (config) => {
-    config.externals.push({
-      '@prisma/client': '@prisma/client',
-    })
-    return config
-  },
 }
 
 module.exports = nextConfig
